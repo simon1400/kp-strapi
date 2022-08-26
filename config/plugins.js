4299c1eb69b2c61dc0e65465488a4be34605d503
Your Name <>
2022-08-26 16:02:57 +0200

    sfgfdg

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,42 @@
+// config/plugins.js
+
 module.exports = {
-  'fast-content': {
-    enabled: true,
+  meilisearch: {
+    config: {
+      produkt: {
+        indexName: "categoryProducts",
+        settings: {
+          filterableAttributes: ['brand', 'values'],
+          // sortableAttributes: [''],
+          searchableAttributes: [
+            'title',
+            'category',
+            'brand',
+            'categorySlug',
+            'brandsSlug',
+            'code',
+            'services',
+            'slug'
+          ],
+        },
+      }
+    }
   },
 }
+
+
+// "categoryProducts": {
+//   query_by: "category,brand,categorySlug,brandsSlug",
+// },
+// "produkties": {
+//   query_by: "title,code,category,brand,categorySlug,brandsSlug"
+// },
+// "categories": {
+//   query_by: "title"
+// },
+// "brands": {
+//   query_by: "title"
+// },
+// "blogs": {
+//   query_by: "title"
+// }
