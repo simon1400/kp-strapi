05c171443e1640e7afaf373b9e590a207f479271
Your Name <>
2022-08-27 11:21:13 +0200

    sdfsdf

--- config/plugins.js
+++ config/plugins.js
@@ -12,30 +12,17 @@ module.exports = {
             'title',
             'category',
             'brand',
-            'categorySlug',
-            'brandsSlug',
             'code',
-            'services',
             'slug'
           ],
         },
-        // transformEntry({ entry }) {
-        //   const data = {
-        //     id: entry.id,
-        //     title: entry?.title,
-        //     logo: entry?.logo,
-        //     content: entry?.content,
-        //     phone: entry?.contact?.phone,
-        //     slug: entry?.slug,
-        //     type: entry?.type,
-        //     rating: entry?.rating || 0,
-        //     address: entry?.address,
-        //     specialisations: entry.specialisations.map(specialisation => specialisation?.title),
-        //     services: entry.services.map(service => service?.title),
-        //     city: entry?.address.split(',')[0].split('г.')[1]
-        //   }
-        //   return data
-        // },
+        transformEntry({ entry }) {
+          const data = {
+            ...entry,
+            categoryTitles: entry.category.map(item => item.title),
+          }
+          return data
+        },
       }
     }
   },
