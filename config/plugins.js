3471e3e1d8a8ddb1c0df6ecd61ca6c9db297b316
Your Name <>
2022-10-06 19:35:54 +0200

    sdfhgsdf

--- config/plugins.js
+++ config/plugins.js
@@ -3,31 +3,6 @@
 module.exports = {
   meilisearch: {
     config: {
-      produkt: {
-        indexName: "categoryProducts",
-        settings: {
-          filterableAttributes: ['brand', 'categoryTitles', 'valuesTitles', 'parameters'],
-          sortableAttributes: ['price'],
-          searchableAttributes: [
-            'title',
-            'categoryTitles',
-            'valuesTitles',
-            "price",
-            'brand',
-            'code',
-            'slug'
-          ],
-        },
-        transformEntry({ entry }) {
-          const data = {
-            ...entry,
-            categoryTitles: entry.category.map(item => item.title),
-            valuesTitles: entry.values.map(item => item.title),
-            price: +entry.price,
-          }
-          return data
-        },
-      },
       category: {
         settings: {
           searchableAttributes: ['title'],
@@ -54,3 +29,30 @@ module.exports = {
     }
   }
 }
+
+
+// produkt: {
+//   indexName: "categoryProducts",
+//   settings: {
+//     filterableAttributes: ['brand', 'categoryTitles', 'valuesTitles', 'parameters'],
+//     sortableAttributes: ['price'],
+//     searchableAttributes: [
+//       'title',
+//       'categoryTitles',
+//       'valuesTitles',
+//       "price",
+//       'brand',
+//       'code',
+//       'slug'
+//     ],
+//   },
+//   transformEntry({ entry }) {
+//     const data = {
+//       ...entry,
+//       categoryTitles: entry.category.map(item => item.title),
+//       valuesTitles: entry.values.map(item => item.title),
+//       price: +entry.price,
+//     }
+//     return data
+//   },
+// },
